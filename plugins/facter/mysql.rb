37cb4d4a8f77ca9afec0fa0fef6092cce9191184
mh <mh@immerda.ch>
2008-09-29 22:14:04 +0000

    fixed it to be usable as well on openbsd

--- plugins/facter/mysql.rb
+++ plugins/facter/mysql.rb
@@ -2,7 +2,7 @@ Facter.add("mysql_exists") do
     ENV["PATH"]="/bin:/sbin:/usr/bin:/usr/sbin"
     
     setcode do
-        mysqlexists = system "which mysql >&/dev/null"
+        mysqlexists = system "which mysql > /dev/null"
         ($?.exitstatus == 0).to_s
     end
 end

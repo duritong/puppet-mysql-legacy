fea257b75321eb82722af149bbb0a77d771f2290
mh <mh@immerda.ch>
2008-09-30 19:51:49 +0000

    make the pipe redirect working on ubuntu, debian, centos, fedora and openbsd

--- plugins/facter/mysql.rb
+++ plugins/facter/mysql.rb
@@ -2,7 +2,7 @@ Facter.add("mysql_exists") do
     ENV["PATH"]="/bin:/sbin:/usr/bin:/usr/sbin"
     
     setcode do
-        mysqlexists = system "which mysql > /dev/null"
+        mysqlexists = system "which mysql > /dev/null 2>&1"
         ($?.exitstatus == 0).to_s
     end
 end

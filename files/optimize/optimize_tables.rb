95c5cd82d9ef2b9b256dddf44531eb0bbf36a09f
mh <mh@immerda.ch>
2009-02-22 13:02:08 +0000

    fixing integer conversion

--- files/optimize/optimize_tables.rb
+++ files/optimize/optimize_tables.rb
@@ -4,7 +4,7 @@ tables = %x{mysql -Bse "SELECT TABLE_SCHEMA,TABLE_NAME FROM information_schema.T
 tables.each { |table|
     tableitems = table.chomp.split(/\t/)
     system "mysql #{tableitems[0]} -Bse \"OPTIMIZE TABLE #{tableitems[1]}\" | grep -q OK"
-    if $?.to_I > 0 then
+    if $?.to_i > 0 then
         puts "error while optimizing #{tableitems[0]}. #{tableitems[1]}"
     end
 }

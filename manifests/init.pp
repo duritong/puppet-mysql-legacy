8100f49e3255ceda036dd107d127aebd914ca054
mh <mh@immerda.ch>
2009-05-19 10:30:24 +0200

    fixing fucked up license

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,18 +2,12 @@
 # mysql module
 #
 # Copyright (C) 2007 David Schmitt <david@schmitt.edv-bus.at>
+# See LICENSE for the full license granted to you.
 # Copyright 2008, admin(at)immerda.ch
 # Copyright 2008, Puzzle ITC GmbH
 # Marcel Härry haerry+puppet(at)puzzle.ch
 # Simon Josi josi+puppet(at)puzzle.ch
 #
-# This program is free software; you can redistribute 
-# it and/or modify it under the terms of the GNU 
-# General Public License version 3 as published by 
-# the Free Software Foundation.
-#
-
-# modules_dir { "mysql": }
 
 class mysql::server {
     case $operatingsystem {

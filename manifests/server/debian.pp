de4c9b3e46a21a37a1081278c68cdf7680c9d15a
Jerome Charaoui <jcharaoui@cmaisonneuve.qc.ca>
2009-12-11 11:33:10 -0500

    manage permissions on debian.cnf

--- manifests/server/debian.pp
+++ manifests/server/debian.pp
@@ -5,4 +5,10 @@ class mysql::server::debian inherits mysql::server::clientpackage {
     File['mysql_ibdata1'] {
         path => '/var/lib/mysql/ibdata1',
     }
+    file { 'mysql_debian_cnf':
+            path => '/etc/mysql/debian.cnf',
+            ensure => file,
+            notify => Service['mysql'],
+            owner => root, group => 0, mode => 0600;
+    }
 }

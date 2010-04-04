d40e1130bc3b8ed5f929406952d1bb598c92a381
mh <mh@immerda.ch>
2010-04-04 12:21:33 +0200

    escape table and db names

--- files/scripts/optimize_tables.rb
+++ files/scripts/optimize_tables.rb
@@ -5,7 +5,7 @@ ENV['HOME'] = '/root'
 tables = %x{mysql -Bse "SELECT TABLE_SCHEMA,TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA NOT IN ('information_schema','mysql') AND Data_free > 0 AND ENGINE IN ('MyISAM','InnoDB','ARCHIVE')"}
 tables.each { |table|
     tableitems = table.chomp.split(/\t/)
-    system "mysql #{tableitems[0]} -Bse \"OPTIMIZE TABLE #{tableitems[0]}.#{tableitems[1]}\" | grep -q OK"
+    system "mysql #{tableitems[0]} -Bse \"OPTIMIZE TABLE \`#{tableitems[0]}\`.\`#{tableitems[1]}\`\" | grep -q OK"
     if $?.to_i > 0 then
         puts "error while optimizing #{tableitems[0]}.#{tableitems[1]}"
     end

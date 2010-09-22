316e75293331d0a9927f0731784c05dd0fb16374
Micah Anderson <micah@riseup.net>
2010-09-22 12:57:16 -0400

    remove license reference from init.pp, as there is no LICENSE file to be referenced

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,6 @@
 # mysql module
 #
 # Copyright (C) 2007 David Schmitt <david@schmitt.edv-bus.at>
-# See LICENSE for the full license granted to you.
 # Copyright 2008, admin(at)immerda.ch
 # Copyright 2008, Puzzle ITC GmbH
 # Marcel Härry haerry+puppet(at)puzzle.ch

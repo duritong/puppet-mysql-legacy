50b6789a3ad88366566c4d8e46567c26091a535f
Micah Anderson <micah@riseup.net>
2010-10-26 11:12:14 -0400

    add the mysql munin graphs:  . mysql_connections  . mysql_qcache  . mysql_qcache_mem  . mysql_size_all

--- manifests/server/munin/debian.pp
+++ manifests/server/munin/debian.pp
@@ -1,8 +1,8 @@
 # manifests/server/munin/debian.pp
 
-class mysql::server::munin::debian {
+class mysql::server::munin::debian inherits mysql::server::munin::base {
     munin::plugin {
-        [mysql_bytes, mysql_queries, mysql_slowqueries, mysql_threads]:
+        [mysql_bytes, mysql_queries, mysql_slowqueries, mysql_threads, mysql_bytes, mysql_queries, mysql_slow_queries, mysql_threads]:
             config => "user root\nenv.mysqlopts --defaults-file=/etc/mysql/debian.cnf",
             require => Package['mysql'],
     }
--- manifests/server/munin/default.pp
+++ manifests/server/munin/default.pp
@@ -1,6 +1,27 @@
 # manifests/server/munin/default.pp
 
-class mysql::server::munin::default {
+class mysql::server::munin::base {
+
+  file {
+    "/usr/local/share/munin-plugins/mysql_connections":
+      source => "$fileserver/munin/mysql_connections",
+      mode => 0755, owner => root, group => root;
+
+    "/usr/local/share/munin-plugins/mysql_qcache":
+      source => "$fileserver/munin/mysql_qcache",
+      mode => 0755, owner => root, group => root;
+
+    "/usr/local/share/munin-plugins/mysql_qcache_mem":
+      source => "$fileserver/munin/mysql_qcache_mem",
+      mode => 0755, owner => root, group => root;
+
+    "/usr/local/share/munin-plugins/mysql_size_all":
+      source => "$fileserver/munin/mysql_size_all",
+      mode => 0755, owner => root, group => root;
+  }
+}
+
+class mysql::server::munin::default inherits mysql::server::munin::base {
     case $munin_mysql_password {
         '': { fail("please specify \$munin_mysql_password to enable mysql munin plugin")}
     }
@@ -16,7 +37,7 @@ class mysql::server::munin::default {
     }
 
     munin::plugin {
-        [mysql_bytes, mysql_queries, mysql_slowqueries, mysql_threads]:
+        [mysql_bytes, mysql_queries, mysql_slowqueries, mysql_threads, mysql_bytes, mysql_queries, mysql_slow_queries, mysql_threads]:
             config => "env.mysqlopts --user=munin --password=${munin_mysql_password} -h localhost",
             require => [ Mysql_grant['munin@localhost'], Mysql_user['munin@localhost'], Package['mysql'] ]
     }

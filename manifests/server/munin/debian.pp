b1ef87fbdacc19fe8f1c56b520e1f870661f688c
Micah Anderson <micah@riseup.net>
2010-10-26 11:27:59 -0400

    fix incorrect munin graph names

--- manifests/server/munin/debian.pp
+++ manifests/server/munin/debian.pp
@@ -2,7 +2,7 @@
 
 class mysql::server::munin::debian inherits mysql::server::munin::base {
     munin::plugin {
-        [mysql_bytes, mysql_queries, mysql_slowqueries, mysql_threads, mysql_bytes, mysql_queries, mysql_slow_queries, mysql_threads]:
+        [mysql_bytes, mysql_queries, mysql_slowqueries, mysql_threads, mysql_connections, mysql_qcache, mysql_cache_mem, mysql_size_all]:
             config => "user root\nenv.mysqlopts --defaults-file=/etc/mysql/debian.cnf",
             require => Package['mysql'],
     }

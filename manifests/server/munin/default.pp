9871e9319c123e2edead294c1645b2baca2fc8a6
Micah Anderson <micah@riseup.net>
2010-10-26 11:28:17 -0400

    move the mysql::server::munin::base class to its own file

--- manifests/server/munin/default.pp
+++ manifests/server/munin/default.pp
@@ -1,26 +1,5 @@
 # manifests/server/munin/default.pp
 
-class mysql::server::munin::base {
-
-  file {
-    "/usr/local/share/munin-plugins/mysql_connections":
-      source => "$fileserver/munin/mysql_connections",
-      mode => 0755, owner => root, group => root;
-
-    "/usr/local/share/munin-plugins/mysql_qcache":
-      source => "$fileserver/munin/mysql_qcache",
-      mode => 0755, owner => root, group => root;
-
-    "/usr/local/share/munin-plugins/mysql_qcache_mem":
-      source => "$fileserver/munin/mysql_qcache_mem",
-      mode => 0755, owner => root, group => root;
-
-    "/usr/local/share/munin-plugins/mysql_size_all":
-      source => "$fileserver/munin/mysql_size_all",
-      mode => 0755, owner => root, group => root;
-  }
-}
-
 class mysql::server::munin::default inherits mysql::server::munin::base {
     case $munin_mysql_password {
         '': { fail("please specify \$munin_mysql_password to enable mysql munin plugin")}

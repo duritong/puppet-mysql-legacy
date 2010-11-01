9583889456e889763d27eed9d88582af6df7dd2e
Micah Anderson <micah@riseup.net>
2010-11-01 19:43:23 -0400

    add mysql munin graphs and adjust the file distribution to match the right location of where they are

--- manifests/server/munin/base.pp
+++ manifests/server/munin/base.pp
@@ -2,19 +2,19 @@ class mysql::server::munin::base {
 
   file {
     "/usr/local/share/munin-plugins/mysql_connections":
-      source => "$fileserver/munin/mysql_connections",
+      source => "puppet:///modules/mysql/munin/mysql_connections",
       mode => 0755, owner => root, group => root;
 
     "/usr/local/share/munin-plugins/mysql_qcache":
-      source => "$fileserver/munin/mysql_qcache",
+      source => "puppet:///modules/mysql/munin/mysql_qcache",
       mode => 0755, owner => root, group => root;
 
     "/usr/local/share/munin-plugins/mysql_qcache_mem":
-      source => "$fileserver/munin/mysql_qcache_mem",
+      source => "puppet:///modules/mysql/munin/mysql_qcache_mem",
       mode => 0755, owner => root, group => root;
 
     "/usr/local/share/munin-plugins/mysql_size_all":
-      source => "$fileserver/munin/mysql_size_all",
+      source => "puppet:///modules/mysql/munin/mysql_size_all",
       mode => 0755, owner => root, group => root;
   }
 }

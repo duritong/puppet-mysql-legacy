53d1455aa55a7d55e858a5b80585df041e7bd19f
mh <mh@immerda.ch>
2012-12-06 23:09:44 +0100

    linting

--- manifests/server/munin/default.pp
+++ manifests/server/munin/default.pp
@@ -1,25 +1,24 @@
-# manifests/server/munin/default.pp
-
+# manage plugins
 class mysql::server::munin::default {
   mysql_user{'munin@localhost':
     password_hash => trocla("mysql_munin_${::fqdn}",'mysql','length: 32'),
-    require => Package['mysql'],
+    require       => Package['mysql'],
   }
 
   mysql_grant{'munin@localhost':
-    privileges => 'select_priv',
-    require => [ Mysql_user['munin@localhost'], Package['mysql'] ],
+    privileges  => 'select_priv',
+    require     => [ Mysql_user['munin@localhost'], Package['mysql'] ],
   }
 
   $munin_mysql_password = trocla("mysql_munin_${::fqdn}",'plain', 'length: 32')
   munin::plugin {
     [mysql_bytes, mysql_queries, mysql_slowqueries, mysql_threads]:
-      config => "env.mysqlopts --user=munin --password=${munin_mysql_password} -h localhost",
+      config  => "env.mysqlopts --user=munin --password='${munin_mysql_password}' -h localhost",
       require => [ Mysql_grant['munin@localhost'], Mysql_user['munin@localhost'], Package['mysql'] ]
   }
 
   Munin::Plugin::Deploy{
-      config => "env.mysqlopts --user=munin --password=${munin_mysql_password} -h localhost",
+      config  => "env.mysqlopts --user=munin --password='${munin_mysql_password}' -h localhost",
       require =>
         [ Mysql_grant['munin@localhost'],
           Mysql_user['munin@localhost'],
@@ -28,7 +27,7 @@ class mysql::server::munin::default {
   munin::plugin::deploy{
     'mysql_connections':
       source => 'mysql/munin/mysql_connections';
-   'mysql_qcache':
+    'mysql_qcache':
       source => 'mysql/munin/mysql_qcache';
     'mysql_qcache_mem':
       source => 'mysql/munin/mysql_qcache_mem';

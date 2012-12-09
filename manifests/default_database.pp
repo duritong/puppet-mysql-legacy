c9fda13929eea1d53e040848ff3693f08d94f80a
mh <mh@immerda.ch>
2012-12-09 16:03:39 +0100

    simplify and correct dependencies
    1. it is important that the root password works
    2. we don't need to chain the dependencies

--- manifests/default_database.pp
+++ manifests/default_database.pp
@@ -12,21 +12,18 @@ define mysql::default_database(
     default => $username
   }
   mysql_database{$name:
-    ensure  => $ensure
+    ensure  => $ensure,
+    require => Exec['mysql_set_rootpw'],
   }
   if $password == 'absent' and $ensure != 'absent' {
       info("we don't create the user for database: ${name}")
-      $grant_require = [ Mysql_database[$name], Exec['mysql_set_rootpw'] ]
+      $grant_require = Mysql_database[$name]
   } else {
     mysql_user{"${real_username}@${host}":
       ensure  => $ensure,
       require => Mysql_database[$name],
     }
-    $grant_require = [
-      Mysql_database[$name],
-      Mysql_user["${real_username}@${host}"],
-      Exec['mysql_set_rootpw']
-    ]
+    $grant_require = Mysql_user["${real_username}@${host}"]
     if $ensure == 'present' {
         $password_hash = $password ? {
           'trocla'  => trocla("mysql_${real_username}",'mysql'),

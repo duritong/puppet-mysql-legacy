6d75999d6ac9a477520ab7f17575434c1f8865c7
mh <mh@immerda.ch>
2013-01-02 18:18:39 +0100

    get rid off trailing ,

--- lib/facter/mysql_version.rb
+++ lib/facter/mysql_version.rb
@@ -1,6 +1,6 @@
 Facter.add("mysql_version") do
     confine :mysql_exists => true
     setcode do
-        Facter::Util::Resolution.exec('mysql --version').chomp.split(' ')[4]
+        Facter::Util::Resolution.exec('mysql --version').chomp.split(' ')[4].split(',').first
     end
 end

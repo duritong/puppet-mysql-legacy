c38546e15fe62e964c63b03222cc3c2824ee5c36
mh <mh@immerda.ch>
2013-12-05 20:14:04 +0100

    introduce the /etc/mysql/conf.d/ directory for CentOS

--- manifests/server/centos.pp
+++ manifests/server/centos.pp
@@ -6,4 +6,21 @@ class mysql::server::centos inherits mysql::server::clientpackage {
   File['mysql_main_cnf']{
     path => '/etc/my.cnf',
   }
+
+  file{
+    '/etc/mysql':
+      ensure  => directory,
+      owner   => root,
+      group   => 0,
+      mode    => '0644';
+    '/etc/mysql/conf.d':
+      ensure  => directory,
+      recurse => true,
+      purge   => true,
+      force   => true,
+      owner   => root,
+      group   => 0,
+      mode    => '0644',
+      notify  => Service['mysql'];
+  }
 }

fd71b9473fcb4c4e9f839bd9e579e899d424b71f
mh <mh@immerda.ch>
2015-01-12 23:29:41 +0100

    split on new line, which fixes an issue if no table is found

--- files/scripts/optimize_tables.rb
+++ files/scripts/optimize_tables.rb
@@ -3,7 +3,7 @@
 # set home as we runit as weekly cron, where HOME is /
 ENV['HOME'] = '/root'
 tables = %x{mysql -Bse "SELECT TABLE_SCHEMA,TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA NOT IN ('information_schema','mysql') AND Data_free > 0 AND ENGINE IN ('MyISAM','InnoDB','ARCHIVE')"}
-tables.each do |table|
+tables.split("\n").each do |table|
   tableitems = table.chomp.split(/\t/)
   output = %x{mysql #{tableitems[0]} -Bse "OPTIMIZE TABLE \\`#{tableitems[0]}\\`.\\`#{tableitems[1]}\\`"  2>&1}
   unless output =~ /status\t+OK/

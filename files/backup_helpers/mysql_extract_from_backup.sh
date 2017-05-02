54810f2e463392e066aba367e889f9162d17d3dc
mh <mh@immerda.ch>
2017-05-02 10:37:31 +0200

    usage should go to stderr

--- files/backup_helpers/mysql_extract_from_backup.sh
+++ files/backup_helpers/mysql_extract_from_backup.sh
@@ -5,8 +5,8 @@ db=$2
 table=$3
 
 if [ ! -f $file ] || [ -z $db ]; then
-  echo "USAGE: $0 file database [table] > dump_file"
-  exit
+  echo "USAGE: $0 file database [table] > dump_file" 1>&2
+  exit 1
 fi
 
 if [ -z $table ]; then
